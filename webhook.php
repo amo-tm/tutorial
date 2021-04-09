dafd8756926d1cd4c0c21d19e1f71e65f96b7a22
meremin <meremin@team.amocrm.com>
2021-04-09 21:02:34 +0300

    webhook endpoint

---
+++ webhook.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * Copyright (c) 2021. AMO | Корпоративный мессенджер.
+ * Это  приложение - демо для документации по API.
+ * Вы можете использовать данный код в своих проектах без сохранения этого копирайта.
+ *
+ * @Author Mike Eremin <meremin@team.amocrm.com>
+ */
+
+require_once 'vendor/autoload.php';
+
+$requestBody = file_get_contents('php://input');
+$parsedBody = json_decode($requestBody, TRUE);
+
+echo "<pre>{$parsedBody}</pre>";
