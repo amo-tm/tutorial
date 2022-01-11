78d7c36a9d2a0b4146a4a26febe511032ad19e88
meremin <meremin@team.amocrm.com>
2022-01-11 16:05:09 +0300

    added front

--- embed_subject.php
+++ embed_subject.php
@@ -124,52 +124,19 @@ if (!$subjectID) {
     $redis->set("T4_SUBJECT_ID", $subjectID);
 }
 
-echo "
-<pre>Team ID: $teamID
-Profile ID: $profileID
-Subject ID: $subjectID
-</pre>
-"
+$subjectService = $teamService->subject($subjectID);
 
-//
-//
-//$subjectService = $teamService->subject();
-//$newSubject = $subjectService->create(new Subject([
-//    'title' => 'A new patient request',
-//    'external_link' => 'https://example.com/',
-//    'author' => Participant::user($createdProfile),
-//    'participants' => new ParticipantCollection([
-//        Participant::user($createdProfile->getId()),
-//    ]),
-//    'subscribers' => new ParticipantCollection([
-//        Participant::user($createdProfile->getId()),
-//    ]),
-//    'threads' => new SubjectThreadCollection([
-//        new SubjectThread([
-//            'title' => 'Patient #100',
-//            'avatar_url' => 'https://picsum.photos/600'
-//        ]),
-//    ]),
-//    'status' => new SubjectStatusCollection([
-//        SubjectStatus::status('Заявка с сайта', '#F9F6EE'),
-//    ])
-//]));
-//
-//$subjectService = $teamService->subject($newSubject->getId());
-//
-//$htmlPath = realpath(__DIR__ . '/embed_subject/dist/index.html');
-//$htmlContent = file_get_contents($htmlPath);
-//
-//$amoWscParams = [
-//  'appId' => $clientId,
-//  'teamId' => $newTeam->getId(),
-//  'subjectId' => $newSubject->getId(),
-//  'userId' => $createdProfile->getId(),
-//  'userToken' => $subjectService->embedUserToken($createdProfile),
-//];
-//
-//$htmlContent = str_replace("\"%AMO_WSC_PARAMS%\"", json_encode($amoWscParams), $htmlContent);
-//
-//echo $htmlContent
+$htmlPath = realpath(__DIR__ . '/embed_subject/dist/index.html');
+$htmlContent = file_get_contents($htmlPath);
 
-?>
+$amoWscParams = [
+  'appId' => $clientId,
+  'teamId' => $teamID,
+  'subjectId' => $subjectID,
+  'userId' => $profileID,
+  'userToken' => $subjectService->embedUserToken($profileID),
+];
+
+$htmlContent = str_replace("\"%AMO_WSC_PARAMS%\"", json_encode($amoWscParams), $htmlContent);
+
+echo $htmlContent;
